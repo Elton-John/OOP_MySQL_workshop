5d06a6d2f92f4005b9aeef7bfa4abb72dce5c8d2
Olga <nauka.dinosaurus@gmail.com>
2020-06-20 17:20:24 +0200

    'findAll' method added

--- src/main/java/UserDao.java
+++ src/main/java/UserDao.java
@@ -1,6 +1,13 @@
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.sql.Connection;
 
 public class UserDao {
+
+    public static Connection connectToWorkshop2() {
+        Connection connToWorkshop2 = DBUtil.connect("workshop2");
+        return connToWorkshop2;
+    }
 
 //    Metoda przyjmuje obiekt klasy
 //, który powinien posiadać wypełnione
@@ -35,10 +42,14 @@ public class UserDao {
 
     public static User read(int userId) {
         Connection connToWorkshop2 = DBUtil.connect("workshop2");
+        User user = getNewUserOfId(userId, connToWorkshop2);
+        return user;
+    }
+
+    private static User getNewUserOfId(int userId, Connection connToWorkshop2) {
         String query = "SELECT email, username, password FROM users WHERE id = " + userId + ";";
         String[] data = DBUtil.readRows(connToWorkshop2, query, "email", "username", "password");
-        User user = new User(data[0], data[1], data[2]);
-        return user;
+        return new User(data[0], data[1], data[2]);
     }
 
 //    Służyć ma ona do odczytania użytkownika dla zadanego identykatora. Jej sygnatura jest
@@ -50,11 +61,34 @@ public class UserDao {
 //    Metoda nic nie zwraca.
 //    W ramach metody należy zmienić dane w bazie na podstawie danych z obiektu.
 
-    public static void update (User user){
+    public static void update(User user) {
         Connection connToWorkshop2 = DBUtil.connect("workshop2");
         String query = "UPDATE users SET email=?, username=?, password=? WHERE id=?;";
-        DBUtil.updateOfId(connToWorkshop2,query,user.getEmail(),user.getUsername(),user.getPassword(),user.getId());
-
+        DBUtil.updateOfId(connToWorkshop2, query, user.getEmail(), user.getUsername(), user.getPassword(), user.getId());
     }
 
+//    ramach metody należy wykonać:
+//    pobrać z bazy danych wszystkie wiersze z tabeli users
+//    na podstawie każdego wiersza utworzyć obiekt klasy
+//         obiekty umieścić w tablicy
+//    zwrócić tablicę obiektów
+//    Będziemy również potrzebować mechanizmu, który pozwoli nam automatycznie powiększać tablicę
+
+    public static User[] findAll() {
+        //Connection connToWorkshop2 = DBUtil.connect("workshop2");
+        User[] users = new User[0];
+        String query = "SELECT id FROM users";
+        int[] tabOfId = DBUtil.getTabOfOneColumn(connectToWorkshop2(), query, "id");
+        for (int userId :
+                tabOfId) {
+            User user = getNewUserOfId(userId, connectToWorkshop2());
+            user.setId(userId);
+            users = ArrayUtils.add(users, user);
+        }
+        for (User userDate :
+                users) {
+            System.out.println(userDate);
+        }
+        return users;
+    }
 }
